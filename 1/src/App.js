a4af8016b47e75447effd1cd76c67206332bf43b
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2023-01-28 03:26:03 +0300

    Styling for Navigation + Logo

--- 1/src/App.js
+++ 1/src/App.js
@@ -1,19 +1,6 @@
 import Home from "./components/routes/home/home.component";
-import { Routes, Route, Outlet } from "react-router-dom";
-
-//--------------------------------------------------------------------------------
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am the Navigation bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
-
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./components/routes/home/navigation/navigation.component";
 //--------------------------------------------------------------------------------
 
 const Shop = () => {
