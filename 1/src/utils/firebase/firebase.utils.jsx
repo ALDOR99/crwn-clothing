77c2911ca112d862a36283b20571460d504d5745
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2023-01-31 20:28:31 +0300

    Sign In With Redirect

--- 1/src/utils/firebase/firebase.utils.jsx
+++ 1/src/utils/firebase/firebase.utils.jsx
@@ -21,18 +21,24 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({
+const GoogleProvider = new GoogleAuthProvider();
+GoogleProvider.setCustomParameters({
   prompt: "select_account",
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, GoogleProvider);
+
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, GoogleProvider);
 
 export const db = getFirestore();
 
 export const createUserDocumentFromAuth = async (userAuth) => {
   const userDocRef = doc(db, "users", userAuth.uid);
+
   console.log(userDocRef);
 
   const userSnapshot = await getDoc(userDocRef);
