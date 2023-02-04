9e64a3d0de2ca85e871b7fcf8a59dcadd75e5b82
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2023-02-04 22:01:43 +0300

    0.1,5/User Context

--- 1/src/components/sign-in-form/sign-in-form.component.jsx
+++ 1/src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 
 import {
   signInWithGooglePopup,
@@ -6,6 +6,8 @@ import {
   signInAuthUserWithEmailAndPassword,
   auth,
 } from "../../utils/firebase/firebase.utils";
+
+import { UserContext } from "../../contexts/user.context";
 
 import { async } from "@firebase/util";
 
@@ -29,6 +31,8 @@ const SignInForm = () => {
 
   const { email, password } = formFields;
 
+  const { setCurrentUser } = useContext(UserContext);
+
   console.log(formFields);
 
   const resetFormFields = () => {
@@ -45,13 +49,11 @@ const SignInForm = () => {
     event.preventDefault();
 
     try {
-      const response = await signInAuthUserWithEmailAndPassword(
+      const { user } = await signInAuthUserWithEmailAndPassword(
         email,
         password
       );
-
-      console.log(response);
-
+      setCurrentUser(user);
       resetFormFields();
     } catch (error) {
       switch (error.code) {
--- 1/src/index.js
+++ 1/src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.scss";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import { render } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { UserProvider } from "./contexts/user.context";
+import "./index.scss";
+
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
 
 //-------------------------------------------------------------------------------------
 
@@ -11,7 +14,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
