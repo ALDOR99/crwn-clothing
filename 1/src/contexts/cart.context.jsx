ae0c7b0fa7e9908ff53db10ef632e0123121a4da
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2023-02-16 21:17:03 +0300

    0.2,2/Add To Cart Pt.2

--- 1/src/contexts/cart.context.jsx
+++ 1/src/contexts/cart.context.jsx
@@ -2,7 +2,24 @@ import { createContext, useState } from "react";
 
 //------------------------------------------------------------------------------
 
-const addCartItem = (cartItems, productToAdd) => {};
+const addCartItem = (cartItems, productToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? {
+            ...cartItem,
+            quantity: cartItem.quantity + 1,
+          }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
 
 export const CartContext = createContext({
   isCartOpen: false,
@@ -21,7 +38,7 @@ export const CartProvider = ({ children }) => {
     setCartItems(addCartItem(cartItems, productToAdd));
   };
 
-  const value = { isCartOpen, setIsCartOpen };
+  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
