90fe29f43444260b5236020a917cdae8eb1e4563
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2023-02-24 21:07:39 +0300

    0.3,8/Styled-Components - Button

--- 1/src/components/sign-in-form/sign-in-form.component.jsx
+++ 1/src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,19 +1,14 @@
 import { useState } from "react";
 
+import FormInput from "../form-input/form-input.component";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
+
 import {
-  signInWithGooglePopup,
-  createUserDocumentFromAuth,
   signInAuthUserWithEmailAndPassword,
-  auth,
+  signInWithGooglePopup,
 } from "../../utils/firebase/firebase.utils";
 
-import { async } from "@firebase/util";
-
-import FormInput from "../form-input/form-input.component";
-
 import "./sign-in-form.styles.scss";
-
-import Button from "../button/button.component";
 
 //--------------------------------------------------------------------------------
 
@@ -68,7 +63,7 @@ const SignInForm = () => {
   //-------------------------------------------------------------------------
 
   return (
-    <div className="sign-up-container">
+    <div className="sign-in-container">
       <h2>Already have an account?</h2>
       <span>Sign in with your email and password</span>
       <form onSubmit={handleSubmit}>
@@ -89,11 +84,14 @@ const SignInForm = () => {
           name="password"
           value={password}
         />
-
         <div className="buttons-container">
           <Button type="submit">Sign In</Button>
-          <Button type="button" buttonType="google" onClick={signInWithGoogle}>
-            Google SIGN IN
+          <Button
+            buttonType={BUTTON_TYPE_CLASSES.google}
+            type="button"
+            onClick={signInWithGoogle}
+          >
+            Sign In With Google
           </Button>
         </div>
       </form>
