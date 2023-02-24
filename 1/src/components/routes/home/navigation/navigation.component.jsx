f0aee8329181a2dd981a1bf56a2184071b368943
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2023-02-24 17:43:17 +0300

    0.3,7 / Introducing Styled-Components

--- 1/src/components/routes/home/navigation/navigation.component.jsx
+++ 1/src/components/routes/home/navigation/navigation.component.jsx
@@ -13,7 +13,12 @@ import { CartContext, CartProvider } from "../../../../contexts/cart.context";
 
 import { signOutUser } from "../../../../utils/firebase/firebase.utils";
 
-import "./navigation.styles.scss";
+import {
+  NavigationContainer,
+  NavLinks,
+  NavLink,
+  LogoContainer,
+} from "./navigation.styles";
 
 //--------------------------------------------------------------------------------
 
@@ -23,31 +28,27 @@ const Navigation = () => {
 
   return (
     <Fragment>
-      <div className="navigation">
-        <Link className="logo-container" to="/">
+      <NavigationContainer>
+        <LogoContainer to="/">
           <CrwnLogo className="logo" />
-        </Link>
+        </LogoContainer>
 
-        <div className="nav-links-container">
-          <Link className="nav-link" to="/shop">
-            SHOP
-          </Link>
+        <NavLinks>
+          <NavLink to="/shop">SHOP</NavLink>
 
           {currentUser ? (
-            <span className="nav-link" onClick={signOutUser}>
+            <NavLink as="span" onClick={signOutUser}>
               SIGN OUT
-            </span>
+            </NavLink>
           ) : (
-            <Link className="nav-link" to="/auth">
-              Sign-In
-            </Link>
+            <NavLink to="/auth">Sign-In</NavLink>
           )}
 
           <CartIcon />
-        </div>
+        </NavLinks>
 
         {isCartOpen && <CartDropdown />}
-      </div>
+      </NavigationContainer>
       <Outlet />
     </Fragment>
   );
