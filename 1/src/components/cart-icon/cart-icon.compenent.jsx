a28a57c915fd23559f9a06eef6214e553fb7b30b
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2023-02-25 21:54:11 +0300

    0.4,0/Styled-Component - Directory Item & Cart Icon

--- 1/src/components/cart-icon/cart-icon.compenent.jsx
+++ 1/src/components/cart-icon/cart-icon.compenent.jsx
@@ -1,8 +1,8 @@
 import { useContext } from "react";
 
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { CartContext } from "../../contexts/cart.context";
-import "./cart-icon.styles.scss";
+
+import { ShoppingIcon, CartIconContainer, ItemCount } from "./cart-icon.styles";
 
 //----------------------------------------------------------------------------
 
@@ -12,11 +12,11 @@ const CartIcon = () => {
   const toogleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
-    <div className="cart-icon-container" onClick={toogleIsCartOpen}>
+    <CartIconContainer onClick={toogleIsCartOpen}>
       <ShoppingIcon className="shopping-icon" />
 
-      <span className="item-count">{cartCount}</span>
-    </div>
+      <ItemCount className="item-count">{cartCount}</ItemCount>
+    </CartIconContainer>
   );
 };
 
