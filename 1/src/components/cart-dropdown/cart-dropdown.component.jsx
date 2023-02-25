bde19e78bfbbd68f1468ba4eef5c874ca7dd74e3
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2023-02-25 20:52:27 +0300

    0.3,9/Styled-Component - Cart Dropdown

--- 1/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ 1/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,17 @@
 import Button from "../button/button.component";
-import "./cart-dropdown.styles.scss";
+
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
 
 import { useNavigate } from "react-router-dom";
 
 import CartItem from "../cart-item/cart-item.component";
+
+import {
+  CartDropdownContainer,
+  EmptyMessage,
+  CartItems,
+} from "./cart-dropdown.styles";
 
 //----------------------------------------------------------------------------
 
@@ -17,14 +23,16 @@ const CartDropdown = () => {
   };
 
   return (
-    <div className="cart-dropdown-container">
-      <div className="cart-items">
-        {cartItems.map((item) => (
-          <CartItem key={item.id} cartItem={item} />
-        ))}
-      </div>
+    <CartDropdownContainer>
+      <CartItems>
+        {cartItems.length ? (
+          cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+        ) : (
+          <EmptyMessage>Your cart is empty</EmptyMessage>
+        )}
+      </CartItems>
       <Button onClick={goToCheckoutHandler}> PAY </Button>
-    </div>
+    </CartDropdownContainer>
   );
 };
 
