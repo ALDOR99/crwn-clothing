afb219a14d92c664d588f2f132bd611a3cf28cb5
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2023-02-26 23:13:04 +0300

    0.4,2/Last Touches

--- 1/src/components/directory-item/directory-item.component.jsx
+++ 1/src/components/directory-item/directory-item.component.jsx
@@ -3,12 +3,18 @@ import {
   Body,
   DirectoryItemContainer,
 } from "./directory-item.styles";
+
+import { useNavigate } from "react-router-dom";
 //--------------------------------------------------------------------------------------
 
 const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route);
+
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={onNavigateHandler}>
       <BackgroundImage imageUrl={imageUrl} />
 
       <Body>
